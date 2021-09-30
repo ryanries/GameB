0e445a4dc0270be0440b364c018c9e0760abfade
Ryan Ries <ryanries09@gmail.com>
2021-09-29 20:54:20 -0500

    Update clang_x64_debug_build.bat

--- clang_x64_debug_build.bat
+++ clang_x64_debug_build.bat
@@ -5,13 +5,15 @@ echo Clang x64 Debug Build Batch Script
 echo ==================================
 echo.
 set WIN_SDK_PATH=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64
-set OUTPUT_PATH=.\x64\Debug\GameB_Clang.exe
+set OUTPUT_PATH=.\x64\Debug
+set OUTPUT_EXE=%OUTPUT_PATH%\GameB_Clang.exe
 @echo on
+mkdir %OUTPUT_PATH%
 clang -g -gcodeview Battle.c CharacterNamingScreen.c ExitYesNoScreen.c GamepadUnplugged.c Main.c ^
 miniz.c NewGameAreYouSure.c OpeningSplashScreen.c OptionsScreen.c Overworld.c stb_vorbis.c TitleScreen.c ^
 -std=c99 -O1 -Wall -march=core-avx2 ^
 -D_DEBUG -DDEBUG -D_WIN64 -D_MBCS -DCLANG ^
--o %OUTPUT_PATH% ^
+-o %OUTPUT_EXE% ^
 -l"%WIN_SDK_PATH%\user32.lib" ^
 -l"%WIN_SDK_PATH%\AdvApi32.lib" ^
 -l"%WIN_SDK_PATH%\Gdi32.lib" ^
@@ -19,5 +21,5 @@ miniz.c NewGameAreYouSure.c OpeningSplashScreen.c OptionsScreen.c Overworld.c st
 @echo off
 if %ERRORLEVEL% NEQ 0 GOTO END
 choice /M "Launch executable under debugger?"
-if %ERRORLEVEL% EQU 1 (start windbgx %OUTPUT_PATH%)
+if %ERRORLEVEL% EQU 1 (start windbgx %OUTPUT_EXE%)
 :END
